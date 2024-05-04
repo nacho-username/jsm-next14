9bc0184558717356cd7636407eee54e273bf368c
Murray Williams <77390255+nacho-username@users.noreply.github.com>
2024-05-04 14:36:44 +0200

    Adds clerk auth

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { UserButton } from '@clerk/nextjs';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1>Home</h1>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 };
 
 export default Home;
