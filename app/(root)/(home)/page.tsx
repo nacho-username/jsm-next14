9e85c55aea33154b4cc1b20d878edbc3d9f9973c
Murray Williams <77390255+nacho-username@users.noreply.github.com>
2024-05-04 17:59:53 +0200

    Adds title + inputs

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,5 +1,32 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import LocalSearch from '@/components/shared/search/LocalSearch';
+
 const Home = () => {
-  return <div>Main Content Section</div>;
+  return (
+    <>
+      <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+
+        <Link href="/ask-question" className="flex justify-end max-sm:w-full">
+          <Button className="primary-gradient !text-light-900 min-h-[46px] px-4 py-3">
+            Ask a Question
+          </Button>
+        </Link>
+      </div>
+
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearch
+          route="/"
+          iconPosition="left"
+          imgSrc="/assets/icons/search.svg"
+          placeholder="Search for questions"
+          otherClasses="flex-1"
+        />{' '}
+        Filters
+      </div>
+    </>
+  );
 };
 
 export default Home;
