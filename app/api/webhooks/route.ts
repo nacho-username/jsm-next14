9d78b93c582890b312bcf76e99791fcdced1341a
Murray Williams <77390255+nacho-username@users.noreply.github.com>
2024-05-07 16:30:11 +0200

    Add console log

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -52,6 +52,8 @@ export async function POST(req: Request) {
   }
 
   const eventType = evt.type;
+
+  console.log(evt.type);
   if (eventType === 'user.created') {
     const { id, email_addresses, image_url, username, first_name, last_name } =
       evt.data;
@@ -65,7 +67,9 @@ export async function POST(req: Request) {
     });
 
     return NextResponse.json({ message: 'OK', user: mongoUser });
-  } else if (eventType === 'user.updated') {
+  }
+
+  if (eventType === 'user.updated') {
     const { id, email_addresses, image_url, username, first_name, last_name } =
       evt.data;
 
