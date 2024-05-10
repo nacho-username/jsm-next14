765e21d83bf109f599a6d3695833249b40160f9a
Murray Williams <77390255+nacho-username@users.noreply.github.com>
2024-05-10 09:58:49 +0200

    Remove experimental options

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverActions: true,
     mdxRs: true,
-    serverComponentsExternalPackages: ['mongoose'],
+    serverComponentsExternalPackages: [],
   },
 };
 
